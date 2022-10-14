f260e8b1b684a848304db62d6d548865f8c822bc
Ximeen <gabriximenes100@gmail.com>
2022-10-13 22:39:05 -0300

    :bug: images bug

--- src/components/Utils/Database.ts
+++ src/components/Utils/Database.ts
@@ -2,23 +2,23 @@ export const ListProjects = [
     {
         name: 'Ignite Lab',
         description: 'Esse foi um projeto desenvolvido no evento ignite Lab, feito pela rocketseat neste projeto utilizei react typescript e tailwind também utilizei graphql foi meu primeiro contato com graphql, gostei  bastante da experiencia.',
-        banner: '../../../public/web_portfolio_ignite_lab.png'
+        banner: '/web_portfolio_ignite_lab.png'
     },
     {
         name: 'Nlw eSports',
         description: 'Foi um dos ultimos eventos do qual eu participei e sem duvidas, foi um dos mais incriveis nele utilizei react, typescript, tailwind e umas das ferramentas mais incriveis, no qual eu fiquei apaixonado que foi o prima e o express, achei sensacional e sem duvidas quero utilizar mais vezes.',
-        banner: '/public/web_portfolio_nlw_eSports.png'
+        banner: '/web_portfolio_nlw_eSports.png'
     },
     {
         name: 'Netflix Clone',
         description: 'Esse projeto tem um tempo, foi uma das minhas primeiras aplicações em react, fiz em uma aula do bonieky lacerda, nela utilizei react, javascript padrão, scss e consumimos a API do the movie database, que foi incrivel.',
-        banner: '/public/web_portfolio_netflix_clone.png'
+        banner: '/web_portfolio_netflix_clone.png'
 
     },
     {
         name: 'Natalino',
         description: 'Neste projeto apenas decidi fazer uma aplicação para treinar minhas habilidades, apenas uma landing page, porém bem moderna e com a opção de mudar o tema da aplicação o que foi muito incrivel de fazer nele utilizei apenas o javascript vanilla, usando a ideia de components e inserindo o HTML pelo javascript.',
-        banner: '/public/web_portfolio_natalino.png'
+        banner: '/web_portfolio_natalino.png'
 
     }
 ]
