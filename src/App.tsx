3a1ac8a516d61863098b8e9bd65bee45c53d8474
Ximeen <gabriximenes100@gmail.com>
2022-10-16 14:18:08 -0300

    :bug: import invalid

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { MyRoutes } from "./components/Utils/MyRoutes";
-import * as Dialog from '@radix-ui/react-dialog'
-import { Projects } from "./components/Projects";
-import { RenderModal } from "./components/CreateModal";
+
 
 
 export function App(){
